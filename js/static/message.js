25358a7d00c8f83f6ea3fa14b57df67646a050e0
sizing-wang <1395041594@qq.com>
2020-10-14 18:18:35 +0800

    搜索

--- js/static/message.js
+++ js/static/message.js
@@ -3,21 +3,22 @@ function ShowTip(tip, type) {
     var $tip = $('#tip');
     if ($tip.length == 0) {
         if (type === 'success') {
-            $tip = $('<span id="tip" style="font-weight:bold;position:fixed;top:10%;left: 50%;z-index:99999999999;background: #cff7e9;border-color: #bcf4e1;color: #08704c;"></span>');
+            $tip = $('<span id="tip" style="font-weight:bold;position:fixed;top:10%;left: 50%;z-index:99999999999;background: #cff7e9;border-color: #bcf4e1;color: #08704c;white-space: nowrap;padding: 0 10px;"></span>');
             $('body').append($tip);
         } else if (type === 'danger') {
-            $tip = $('<span id="tip" style="font-weight:bold;position:fixed;top:10%;left: 50%;z-index:99999999999;background: #fdd8dd;border-color: #fcc8cf;color: #801f2c"></span>');
+            $tip = $('<span id="tip" style="font-weight:bold;position:fixed;top:10%;left: 50%;z-index:99999999999;background: #fdd8dd;border-color: #fcc8cf;color: #801f2c;white-space: nowrap;padding: 0 10px;"></span>');
             $('body').append($tip);
         } else if (type === 'info') {
-            $tip = $('<span id="tip" style="font-weight:bold;position:fixed;top:10%;left: 50%;z-index:99999999999;background: #1890ff"></span>');
+            $tip = $('<span id="tip" style="font-weight:bold;position:fixed;top:10%;left: 50%;z-index:99999999999;background: #1890ff;white-space: nowrap;padding: 0 10px;"></span>');
             $('body').append($tip);
         } else if (type === 'warning') {
-            $tip = $('<span id="tip" style="font-weight:bold;position:fixed;top:10%;left: 50%;z-index:99999999999;background: #faad14"></span>');
+            $tip = $('<span id="tip" style="font-weight:bold;position:fixed;top:10%;left: 50%;z-index:99999999999;background: #faad14;white-space: nowrap;padding: 0 10px;"></span>');
             $('body').append($tip);
         }
 
     }
-    $tip.stop(true).attr('class', 'alert alert-' + type).text(tip).css('margin-left', -$tip.outerWidth() / 2).fadeIn(500).delay(2000).fadeOut(500);
+    /* $tip.stop(true).attr('class', 'alert alert-' + type).text(tip).css('margin-left', -$tip.outerWidth() / 2).fadeIn(500).delay(2000).fadeOut(500); */
+    $tip.stop(true).attr('class', 'alert alert-' + type).text(tip).css('transform', 'translateX(-50%)').fadeIn(500).delay(2000).fadeOut(500);
 }
 
 function ShowMsg(msg) {
