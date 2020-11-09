770e9dde5605d7c3187c373f3406d844f0078333
sizing-wang <1395041594@qq.com>
2020-11-09 18:16:12 +0800

    搜索

--- js/search.js
+++ js/search.js
@@ -8,46 +8,80 @@ let page = {
         /**
          *  获取关键字
          */
-        /*
-        $.ajax({
-            type: "",
-            url: '',
+        console.log('12321------')
+        // let keyWordList = []
+            $.ajax({
+            type: "get",
+            url: 'http://61.147.123.84:9000/v2/searchKeyword',
             data: {},
             dataType: 'json',
+            contentType: 'application/json',
             success: function (result) {
-                console.log(result);
+                console.log(222222222222, result);
+                if (result.msg = 'success') {
+                    // keyWordList = result.data
+                    console.log(111111111, keyWordList);
+                    /*
+                    for (let i = 0; i < 3; i++) {
+                        let html = ''
+                        if (i === 0) {
+                            for (let j = 0; j < 5; j++) {
+                                html += `
+                                    <div id="keyword_btn${j}">${keyWordList[j]}</div>
+                                `
+                            }
+                            $('.htmleaf-container1 .slider').html(html)
+                        } else if (i === 1) {
+                            for (let j = 5; j < 10; j++) {
+                                html += `
+                                    <div id="keyword_btn${j}">${keyWordList[j]}</div>
+                                `
+                            }
+                            $('.htmleaf-container2 .slider').html(html)
+                        } else if (i === 2) {
+                            for (let j = 10; j < 12; j++) {
+                                html += `
+                                    <div id="keyword_btn${j}">${keyWordList[j]}</div>
+                                `
+                            }
+                            $('.htmleaf-container3 .slider').html(html)
+                        }
+                    }
+                    */
+                }
             },
-            error: function () {
+            error: function (err) {
                 ShowFailure('网络错误, 请稍后再试 !')
             }
         })
-        */
-        let keyWordList = ['关键字0', '关键字1', '关键字2', '关键字3', '关键字4', '关键字5', '关键字6', '关键字7', '关键字8', '关键字9', '关键字10', '关键字11', '关键字12', '关键字13', '关键字14']
+        let keyWordList = ['yzwc43', '2u', 'sang', 'doutinghao26', 'hfh06', 'yzwc40', 'zcs02', 'jinxiu25', 'MLH15', 'hfh07', 'yzwc39', '7yi']
         for (let i = 0; i < 3; i++) {
             let html = ''
             if (i === 0) {
                 for (let j = 0; j < 5; j++) {
                     html += `
-                        <div id="keyword_btn${j}">${keyWordList[j]}</div>
-                    `
+                                    <div id="keyword_btn${j}">${keyWordList[j]}</div>
+                                `
                 }
                 $('.htmleaf-container1 .slider').html(html)
             } else if (i === 1) {
                 for (let j = 5; j < 10; j++) {
                     html += `
-                        <div id="keyword_btn${j}">${keyWordList[j]}</div>
-                    `
+                                    <div id="keyword_btn${j}">${keyWordList[j]}</div>
+                                `
                 }
                 $('.htmleaf-container2 .slider').html(html)
             } else if (i === 2) {
-                for (let j = 10; j < 15; j++) {
+                for (let j = 10; j < 12; j++) {
                     html += `
-                        <div id="keyword_btn${j}">${keyWordList[j]}</div>
-                    `
+                                    <div id="keyword_btn${j}">${keyWordList[j]}</div>
+                                `
                 }
                 $('.htmleaf-container3 .slider').html(html)
             }
         }
+
+
     },
     handleKeyWord: function () {
         /*
@@ -125,23 +159,21 @@ let page = {
          *  搜索
          */
         $('.searchButton').on('click', function () {
-            let val = $('.keyWord_ipt').val()
-            console.log('----------', val);
-            /*
+            let name = $('.keyWord_ipt').val()
+            console.log('----------', name);
             $.ajax({
-                type: "",
-                url: '',
-                data: {},
+                type: "get",
+                url: 'http://61.147.123.84:9000/v2/searchFile',
+                data: {name},
                 dataType: 'json',
                 success: function (result) {
-
-                    console.log(result);
+                    console.log(':::::::::------------', result);
                 },
                 error: function () {
                     ShowFailure('网络错误, 请稍后再试 !')
                 }
             })
-            */
+
         })
     }
 }
